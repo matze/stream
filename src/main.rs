e35305948f687df240f09d628a2a948958536d3b
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-04-06 23:01:40 +0200

    Serve track with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
 mod storage;
 mod tcx;
 
 use anyhow::Result;
 use askama::Template;
+use chrono::prelude::*;
+use rocket::State;
+use std::collections::HashMap;
 
 struct Sample {
     position: geo::Point<f64>,
     heart_rate: i32,
 }
 
-#[derive(Template)]
-#[template(path = "index.html")]
-struct RenderTemplate {
-    samples: Vec<Sample>,
-}
-
 impl Sample {
-    fn from(track_point: tcx::TrackPoint) -> Option<Self> {
+    fn from(track_point: &tcx::TrackPoint) -> Option<Self> {
         if let Some(position) = track_point.position {
             Some(Self {
                 position: position,
@@ -28,14 +30,36 @@ impl Sample {
     }
 }
 
-fn main() -> Result<()> {
-    let activities = storage::load("storage")?;
-    let activity = activities.into_iter().map(|a| a.1).nth(0).unwrap();
-    let points = itertools::concat(activity.laps.into_iter().map(|l| l.track_points));
-    let samples: Vec<Sample> = points.into_iter().filter_map(|p| Sample::from(p)).collect();
-    let template = RenderTemplate { samples };
+#[derive(Template)]
+#[template(path = "index.html")]
+struct RenderTemplate {
+    samples: Vec<Sample>,
+}
 
-    println!("{}", template.render()?);
+type ActivityMap = HashMap<chrono::DateTime<Utc>, tcx::Activity>;
+
+#[get("/")]
+fn index(activities: State<ActivityMap>) -> RenderTemplate {
+    let activity = activities.iter().map(|a| a.1).nth(0).unwrap();
+
+    let mut samples = Vec::new();
+
+    for lap in &activity.laps {
+        for point in &lap.track_points {
+            if let Some(sample) = Sample::from(&point) {
+                samples.push(sample);
+            }
+        }
+    }
+
+    RenderTemplate { samples }
+}
+
+fn main() -> Result<()> {
+    rocket::ignite()
+        .manage(storage::load("storage")?)
+        .mount("/", routes![index])
+        .launch();
 
     Ok(())
 }
