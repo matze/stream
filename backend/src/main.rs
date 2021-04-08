82ef6167bd535cd5905503db5ac50e5006c62cd1
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-04-08 20:09:53 +0200

    Add Yew frontend skeleton and serve with Rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,55 +9,24 @@ mod tcx;
 use anyhow::Result;
 use askama::Template;
 use chrono::prelude::*;
-use rocket::State;
+use rocket_contrib::serve::StaticFiles;
 use std::collections::HashMap;
-
-struct Sample {
-    position: geo::Point<f64>,
-    heart_rate: i32,
-}
-
-impl Sample {
-    fn from(track_point: &tcx::TrackPoint) -> Option<Self> {
-        if let Some(position) = track_point.position {
-            Some(Self {
-                position: position,
-                heart_rate: track_point.heart_rate,
-            })
-        } else {
-            None
-        }
-    }
-}
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct RenderTemplate {
-    samples: Vec<Sample>,
-}
+struct IndexTemplate {}
 
 type ActivityMap = HashMap<chrono::DateTime<Utc>, tcx::Activity>;
 
 #[get("/")]
-fn index(activities: State<ActivityMap>) -> RenderTemplate {
-    let activity = activities.iter().map(|a| a.1).nth(0).unwrap();
-
-    let mut samples = Vec::new();
-
-    for lap in &activity.laps {
-        for point in &lap.track_points {
-            if let Some(sample) = Sample::from(&point) {
-                samples.push(sample);
-            }
-        }
-    }
-
-    RenderTemplate { samples }
+fn index() -> IndexTemplate {
+    IndexTemplate {}
 }
 
 fn main() -> Result<()> {
     rocket::ignite()
         .manage(storage::load("storage")?)
+        .mount("/static", StaticFiles::from("static"))
         .mount("/", routes![index])
         .launch();
 
