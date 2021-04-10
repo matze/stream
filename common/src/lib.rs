ea966aec768bcc7cd11f5cb0865539e1b0e27c7a
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-04-11 01:01:29 +0200

    Add actual shared code

---
+++ common/src/lib.rs
@@ -0,0 +1,31 @@
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+use uom::si::f32::{Length, Time};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Activities {
+    /// Maps datetime Id to sport type
+    pub ids: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TrackPoint {
+    pub time: chrono::DateTime<Utc>,
+    pub heart_rate: i32,
+    pub position: Option<geo::Point<f64>>,
+    pub sensor_state: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Lap {
+    pub total_time: Time,
+    pub distance: Length,
+    pub track_points: Vec<TrackPoint>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Activity {
+    pub sport: String,
+    pub id: chrono::DateTime<Utc>,
+    pub laps: Vec<Lap>,
+}
